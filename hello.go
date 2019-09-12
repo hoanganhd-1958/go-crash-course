176bc3790488cbdba38fc5e1d428139f50bf40ad
hoanganhhd-1958 <doan.hoang.anh@sun-asterisk.com>
2019-09-12 17:28:31 +0700

    func

--- hello.go
+++ hello.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Hello")
 	variable()
+	fmt.Println(calculateBill(1000, 21))
 }
+
 func variable() {
 	var age int
 	age = 21
@@ -13,4 +15,26 @@ func variable() {
 	name, age := "naveen", 90
 
 	fmt.Println("my name is", name, "age is", age)
+	area, perimeter := rectProps(10.8, 5.6)
+	fmt.Printf("Area %f Perimeter %f", area, perimeter)
+
+	area, _, h := rectProps2(10.8, 5.6, 2.3)
+	fmt.Printf("Area %f Perimeter %f", area, h)
+}
+
+func calculateBill(price int, no int) int {
+	var totalPrice = price * no
+	return totalPrice
+}
+
+func rectProps(length, width float64) (float64, float64) {
+	var area = length * width
+	var perimeter = (length + width) * 2
+	return area, perimeter
+}
+
+func rectProps2(length, width float64, h float64) (float64, float64, float64) {
+	var area = length * width
+	var perimeter = (length + width) * 2
+	return area, perimeter, h
 }
